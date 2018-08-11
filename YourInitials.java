9a931da0d0048b6d77de986e8bdd728e7d3119c3
shyam81295 <shyam81295@gmail.com>
2018-08-12 03:36:25 +0530

    5

---
+++ YourInitials.java
@@ -0,0 +1,11 @@
+public class YourInitials{
+    public static void main( String args[] ){
+        System.out.println(" SSS   DDDD    SSS ");
+        System.out.println("S   S  D   D  S   S");
+        System.out.println("S      D   D  S    ");
+        System.out.println(" SSS   D   D   SSS ");
+        System.out.println("    S  D   D      S");
+        System.out.println("S   S  D   D  S   S");
+        System.out.println(" SSS   DDDD    SSS ");
+    }
+}
