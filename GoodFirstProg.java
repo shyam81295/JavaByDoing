7704d3617b20ce0d507054ecd19a93ed2383b0cc
shyam81295 <shyam81295@gmail.com>
2018-08-12 02:44:05 +0530

    2

---
+++ GoodFirstProg.java
@@ -0,0 +1,12 @@
+public class GoodFirstProg{
+    public static void main( String args[] ){
+        // 2 forward slashes gives a single-line comment.
+        System.out.println( "Hello World!" );
+        System.out.println( "Hello Again!" );
+        System.out.println( "I like typing this." );
+        System.out.println( "This is fun." );
+        System.out.println( "Yay! Printing." );
+        System.out.println( "I'd much rather you 'not'." );
+        System.out.println( "I \"said\" do not touch this." );
+    }
+}
