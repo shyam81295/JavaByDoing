34c31ac23971ee5e929d4b6c3fa306b41b994e99
shyam81295 <shyam81295@gmail.com>
2018-08-13 01:26:06 +0530

    6

---
+++ NumbersAndMath.java
@@ -0,0 +1,36 @@
+// This class demonstrates how we deal with arithmetic operators
+// in Java.
+public class NumbersAndMath{
+    public static void main( String args[] ){
+        System.out.println("I will now count my chickens:");
+
+        // '/' has more precedence than '+'
+        System.out.println("Hens " + ( 25 + 30 / 6 ) );
+        // '*' > '%' > '-'
+        System.out.println("Roosters " + ( 100 -25 * 3 % 4 ) );
+
+        System.out.println("Now I will count the eggs:");
+
+        // '/' > '%' > '+' = '-'
+        // '1 / 4' == 0
+        // '4 % 2' == 0
+        System.out.println( 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6 );
+
+        System.out.println("Is it true that 3 + 2 < 5 - 7?");
+
+        // Arithmetic operators has more precedence than comparison operators
+        System.out.println( 3 + 2 < 5 - 7 );
+
+        System.out.println("What is 3 + 2?" + ( 3 + 2 ) );
+        System.out.println("What is 5 - 7?" + ( 5 - 7 ) );
+
+        System.out.println("Oh, that's why it's false." );
+
+        System.out.println("How about some more." );
+
+        // Comparison operators returns boolean result
+        System.out.println("Is it greater? " + ( 5 > -2 ) );
+        System.out.println("Is it greater or equal? " + ( 5 >= -2 ) );
+        System.out.println("Is it less or equal? " + ( 5 <= -2 ) );  
+    }
+}
